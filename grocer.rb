92a3cf2ed1f22456edf73a8608f701feff7c38a5
a lan <bluenowte97@gmail.com>
2018-11-17 18:34:40 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -12,20 +12,28 @@ cart_hash = {}
 cart_hash
 end
 
-# #works in base case if coupon is applicable, if coupon applies to 3 items, and only 1 item in cart,
-# def apply_coupons(cart, coupons)
-# #issue is conversion of hash into integer as the coupon hash is array ,#find a way to save the selected_coupon
-# #into its own array with 1 item. Then refer to it as selected_coupon[0]
-#   old_count = 0
-#   selected_coupon = coupons.select do |element|
-#     selected_item = cart.select do |cart_key, cart_value|
-#       old_count = cart_value[:count]
-#       cart_key == element[:item]
-#         if element[:num] <= cart_value[:count]
-#            cart_value[:count] = cart_value[:count] - element[:num]
-#         end
-#      end
-#    end
+#works in base case if coupon is applicable, if coupon applies to 3 items, and only 1 item in cart,
+def apply_coupons(cart, coupons)
+#issue is conversion of hash into integer as the coupon hash is array ,#find a way to save the selected_coupon
+#into its own array with 1 item. Then refer to it as selected_coupon[0]
+  old_count = 0
+  selected_coupon = coupons.select do |element|
+    selected_item = cart.select do |cart_key, cart_value|
+      old_count = cart_value[:count]
+      cart_key == element[:item]
+        if cart_key && if element[:num] <= cart_value[:count]
+           cart_value[:count] =cart_value[:count] - element[:num]
+           food_key = cart_key + " W/COUPON"
+           if cart[food_key]
+             cart[food_key][:count]+= 1
+           else
+             cart[food_key] = {count => 1, price => element[]:cost]}
+             cart[food_key][:clearance] = cart[cart_key]:clearance]
+          end
+        end
+      end  
+   end
+ end 
 #    if selected_coupon.length != 0
 #    food_key = selected_coupon[0][:item] + " W/COUPON"
 #    cart[food_key] = {}
@@ -42,21 +50,21 @@ end
 # end
 
 #better solution
-def apply_coupons(cart, coupons)
-  coupons.each do |element|
-    name = element[:item]
-    if cart[name] && cart[name][:count] >= element[:num]
-      if cart["#{name} W/COUPON"]
-        cart["#{name} W/COUPON"][:count] += 1
-      else
-        cart["#{name} W/COUPON"] = {:count => 1, :price => element[:cost]}
-        cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
-      end
-      cart[name][:count] -= element[:num]
-    end
-  end
-  cart
-end
+# def apply_coupons(cart, coupons)
+#   coupons.each do |element|
+#     name = element[:item]
+#     if cart[name] && cart[name][:count] >= element[:num]
+#       if cart["#{name} W/COUPON"]
+#         cart["#{name} W/COUPON"][:count] += 1
+#       else
+#         cart["#{name} W/COUPON"] = {:count => 1, :price => element[:cost]}
+#         cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
+#       end
+#       cart[name][:count] -= element[:num]
+#     end
+#   end
+#   cart
+# end
 
 def apply_clearance(cart)
   cart.each do |key, value|
